fcc507b5fe72a315ed0a61933e6ebce1a9ca3641
Ryan Castelli <ryancastelli19@gmail.com>
2024-11-28 00:51:00 -0300

    adicionando endereços

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import SignupPage from "./pages/Signup";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <LandingPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<SignupPage/>}/>
+        <Route path="/*" element={<LandingPage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
